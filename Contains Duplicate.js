99c9da87634cb5bae7674fc6b2b2d8e00167c9cc
deepakmardii <deepakmardi22@gmail.com>
2023-02-10 07:15:37 +0530

    Initial commit

---
+++ Contains Duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    for(let right=0; right<nums.length; right++){
+        for(let left=0; left<right; left++){
+            const isDuplicate = nums[left] == nums[right];
+            if(isDuplicate){
+                return true;
+            }
+        }
+    }
+    return false;
+};
